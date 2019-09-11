3f4834df4c6ea728afe8940321c93a04782c4a76
Wang, Ting <Ting.Wang@anthem.com>
2019-09-11 13:37:25 -0500

    add cert

--- src/db/config.js
+++ src/db/config.js
@@ -6,7 +6,8 @@ var config = {
         database: {
             host:   '127.0.0.1',
             port:   '27017',
-            db:     'step-forward'
+            db:     'step-forward',
+            urlConnection : 'mongodb://127.0.0.1:27017/step-forward'
         },
         //server details
         server: {
@@ -21,7 +22,9 @@ var config = {
         database: {
             host: '127.0.0.1',
             port: '27017',
-            db:     'site'
+            db:     'site',
+            urlConnection : 'mongodb://srccogtrw:AhdS592JpQ@va33dlvmdb322.wellpoint.com:37043/cognativedb?ssl=true&ssl_ca_certs=./cert/root_chain.pem'
+ 
         },
         //server details
         server: {
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose")
 var env = process.env.NODE_ENV || 'dev';
 var config = require('./config')[env];
 
-const connectionURL = "mongodb://" + config.database.host + ":" + config.database.port + "/" + config.database.db
+// const connectionURL = "mongodb://" + config.database.host + ":" + config.database.port + "/" + config.database.db
 
-console.log(connectionURL)
+console.log("I am here")
+const url = config.database.urlConnection
+console.log("ending here")
+console.log(url)
 
-mongoose.connect(connectionURL, {
+mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
